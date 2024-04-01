cbeab70410a2d02a4c804006b37d704b10281ab8
yogeshrasam <ygsh.rasam@gmail.com>
2024-04-01 19:32:40 +0530

    Confusing method names

--- RD.Rental.Repository/Repo/MockRepo.cs
+++ RD.Rental.Repository/Repo/MockRepo.cs
@@ -42,4 +42,21 @@ namespace RD.Rental.Repository.Repo
             }       
         }
     }
+
+    class Date
+    {
+        private int year;
+        private int month;
+        private int day;
+
+        public String ConvertToISO()
+        {
+            return year + "-" + month + "-" + day;
+        }
+
+        public String ConvertToIso()
+        {
+            return year + "-" + month + "-" + day;
+        }
+    }
 }
